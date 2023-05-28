059de1eaf9ed0d326a3f7b152897985db4803951
Thiago Ker <124298988+ThiagoKer@users.noreply.github.com>
2023-05-27 23:33:20 -0300

    Add files via upload

--- codigo2.sql
+++ codigo2.sql
@@ -1,14 +1,14 @@
---Apaga o banco de dados se ele já for existente
+--Apaga o banco de dados "uvv" se ele já for existente
 DROP DATABASE IF EXISTS uvv;
 
---Apaga o usuario se ele já for existente
+--Apaga o usuario "thiago" se ele já for existente
 DROP USER IF EXISTS thiago;
 
---cria o usuario
+--cria o usuario "thiago"
 CREATE USER thiago with CREATEDB INHERIT login password '123';
 
 
---cria um banco de dados 
+--cria o banco de dados "uvv" 
 CREATE DATABASE uvv 
 	OWNER thiago
 	TEMPLATE template0 
@@ -17,6 +17,7 @@ CREATE DATABASE uvv
 	LC_CTYPE 'pt_BR.UTF-8'
 	ALLOW_CONNECTIONS TRUE;
 
+--Usa o banco de dados "uvv" e o usuário "thiago" sem pedir a senha de login
 \c 'dbname=uvv user=thiago password=123';
 
 
@@ -63,7 +64,7 @@ CREATE TABLE produtos (
                 nome                                    VARCHAR(255)      NOT NULL,
                 preco_unitario                          NUMERIC(10,2),
                 detalhes                                BYTEA,
-                magem                                   BYTEA,
+                imagem                                  BYTEA,
                 imagem_mimi_type                        VARCHAR(512),
                 imagem_arquivo                          VARCHAR(512),
                 imagem_charset                          VARCHAR(512),
@@ -163,7 +164,9 @@ COMMENT ON COLUMN pedidos_itens.preco_unitario          IS      'Preço unitári
 COMMENT ON COLUMN pedidos_itens.quantidade              IS      'Quantidade de pedidos feitos';
 COMMENT ON COLUMN pedidos_itens.envio_id                IS      'Número de identificação dos envios';
 
+/*Criação das Foreign Keys das tabelas do banco de dados "uvv"*/
 
+--Cria a Foreign Key da tabela envios
 ALTER TABLE envios ADD CONSTRAINT lojas_envios_fk
 FOREIGN KEY (loja_id)
 REFERENCES lojas (loja_id)
@@ -171,6 +174,7 @@ ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
+--Cria a Foreign Key da tabela estoques
 ALTER TABLE estoques ADD CONSTRAINT lojas_estoques_fk
 FOREIGN KEY (loja_id)
 REFERENCES lojas (loja_id)
@@ -178,6 +182,7 @@ ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
+--Cria a Foreign Key da tabela pedidos
 ALTER TABLE pedidos ADD CONSTRAINT lojas_pedidos_fk
 FOREIGN KEY (loja_id)
 REFERENCES lojas (loja_id)
@@ -185,6 +190,7 @@ ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
+--Cria a Foreign Key da tabela estoques
 ALTER TABLE estoques ADD CONSTRAINT produtos_estoques_fk
 FOREIGN KEY (produto_id)
 REFERENCES produtos (produto_id)
@@ -192,6 +198,8 @@ ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
+
+--Cria a Foreign Key da tabela pedidos_itens
 ALTER TABLE pedidos_itens ADD CONSTRAINT produtos_pedidos_itens_fk
 FOREIGN KEY (produto_id)
 REFERENCES produtos (produto_id)
@@ -199,6 +207,7 @@ ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
+--Cria a Foreign Key da tabela pedidos
 ALTER TABLE pedidos ADD CONSTRAINT clientes_pedidos_fk
 FOREIGN KEY (cliente_id)
 REFERENCES clientes (cliente_id)
@@ -206,6 +215,7 @@ ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
+--Cria a Foreign Key da tabela envios
 ALTER TABLE envios ADD CONSTRAINT clientes_envios_fk
 FOREIGN KEY (cliente_id)
 REFERENCES clientes (cliente_id)
@@ -213,6 +223,7 @@ ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
+--Cria a Foreign Key da tabela pedidos_itens
 ALTER TABLE pedidos_itens ADD CONSTRAINT envios_pedidos_itens_fk
 FOREIGN KEY (envio_id)
 REFERENCES envios (envio_id)
@@ -220,6 +231,8 @@ ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
+
+--Cria a Foreign Key da tabela pedidos_itens
 ALTER TABLE pedidos_itens ADD CONSTRAINT pedidos_pedidos_itens_fk
 FOREIGN KEY (pedido_id)
 REFERENCES pedidos (pedido_id)
@@ -227,12 +240,22 @@ ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
+/*Adiciona restrições as tabelas determinadas para não serem inseridados dados invalidados nas tabelas */
 
-
+--Restrição da tabela produtos para o preço não ser negativo
 ALTER TABLE produtos  ADD CONSTRAINT preco_positivo       CHECK(preco_unitario >= 0);
+
+--Restrição da tabela estoques para a quantidade não ser negativa
 ALTER TABLE estoques  ADD CONSTRAINT quantidade_positiva  CHECK(quantidade >= 0);
+
+--
 ALTER TABLE pedidos   ADD CONSTRAINT status_certo         CHECK(status IN ('CANCELADO, COMPLETO, ABERTO, PAGO, REEMBOLSADO, ENVIADO'));
+
+--
 ALTER TABLE envios    ADD CONSTRAINT status_certo_2       CHECK(status IN('CRIADO, ENVIADO, TRANSITO, ENTREGUE'));
+
+-- Comando para não deixar a tabela endeco_web e endereco_fisico vazias ao mesmo tempo
+ALTER TABLE lojas     ADD CONSTRAINT endereco_vazio 	  CHECK(endereco_web IS not null OR endereco_fisico IS not null);
 
 
 
